31f4c82ed11f629f8ffc73c429c92d1e7507aa25
vaveyko <zakhvey.ivan@gmail.com>
2026-04-29 22:24:11 +0300

    DS-4.1 update run_pipeline, add using of new model functions fit_model and make_predict

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,18 +1,19 @@
 from scripts.ETL import main as preprocess_data
-from pandas import read_csv
-from scripts.Validation import ModelValidation, fit_validate, RMSE, MAE, MSE
-from sklearn.linear_model import LinearRegression
+from scripts.Model import fit_model, make_predict
 
 
 if __name__ == "__main__":
     print("Start Preprocessing data".center(40, "="))
     preprocess_data("data")
 
-    print("Start fit and validate model".center(40, "="))
-    df = read_csv("data/preprocessed/sales_train_preprocessed.csv")
-    val_model = ModelValidation(df, ["item_cnt_month"])
+    print("Start fit model".center(40, "="))
+    model = fit_model("data/preprocessed/sales_train_preprocessed.csv")
 
-    # use 33 because we have data of 33 month
-    splitter = val_model.split_data(n_splits=33, window_type="expanding")
-    model = LinearRegression()
-    fit_validate(model, splitter, [RMSE, MAE, MSE], val_model, True)
+    print("Start prediction".center(40, "="))
+    make_predict(
+        model,
+        "data/preprocessed/test_preprocessed.csv",
+        "data/preprocessed/sales_test_answer.csv",
+    )
+
+    print("FINISHED".center(40, "="))
